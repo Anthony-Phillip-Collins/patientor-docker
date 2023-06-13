1333fb920299c7b9268d6c55ea187d052806618a
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-06-13 15:59:44 +1000

    removed redundant projects

--- fligh-diary/frontend/src/types/IDiaryEntry.ts
+++
@@ -1,9 +0,0 @@
-export interface IDiaryEntry {
-  id: number;
-  date: string;
-  weather: string;
-  visibility: string;
-  comment: string;
-}
-
-export type IDiaryEntryNew = Omit<IDiaryEntry, "id">;
--- fligh-diary/frontend/src/util/DateUtil.ts
+++
@@ -1,8 +0,0 @@
-export const dateToString = (date: Date) => {
-  const toDoubleDigitString = (value: number): string =>
-    value < 10 ? "0" + value.toString() : value.toString();
-  const year = date.getFullYear().toString();
-  const month = toDoubleDigitString(date.getMonth());
-  const day = toDoubleDigitString(date.getDay());
-  return `${year}-${month}-${day}`;
-};
